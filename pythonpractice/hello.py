68ba888926aff769ebcb7d7376528e51b58110ed
ayaan-gupta1644 <ayaangupta79080@gmail.com>
2025-09-16 10:42:06 +0530

    game

--- pythonpractice/hello.py
+++ pythonpractice/hello.py
@@ -1,9 +1,15 @@
-print("hELLO WORLD")
-print("Hello World") #better way to write
-print("ayaan is very very cool")
-print("i love atishay")
-print(3+5)
-print(3*5)
-print(3/5)
-print(3-5)
-print("ayaan")
+import random
+a=random.randrange(1,50)
+n=int(input("Print a number between 1 to 50: " ))
+if n==a:
+    print("You win")    
+else:
+    n2=int(input("Try again: "))
+    if n2==a:
+        print("You win")
+    else:
+        n3=int(input("Last chance: "))
+        if n3==a:
+            print("You win")
+        else:
+            print("You lose, the number was ",a)
